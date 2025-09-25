2ece1688c35300a076964426eaf42e0d4530ebe5
jsiryj <jsiryj@gmail.com>
2025-09-25 17:49:04 +0000

    Add DuckDB table

--- dagster_university/dagster_and_etl/src/dagster_and_etl/defs/assets.py
+++ dagster_university/dagster_and_etl/src/dagster_and_etl/defs/assets.py
@@ -1,4 +1,6 @@
 import dagster as dg
+from dagster_duckdb import DuckDBResource
+
 from pathlib import Path
 
 class IngestionFileConfig(dg.Config):
@@ -10,3 +12,26 @@ def import_file(context: dg.AssetExecutionContext, config: IngestionFileConfig) 
         Path(__file__).absolute().parent / f"../../../data/source/{config.path}"
     )
     return str(file_path.resolve())
+
+@dg.asset(
+    kinds={"duckdb"},
+)
+def duckdb_table(
+    context: dg.AssetExecutionContext,
+    database: DuckDBResource,
+    import_file,
+):
+    table_name = "raw_data"
+    with database.get_connection() as conn:
+        table_query = f"""
+            create table if not exists {table_name} (
+                date date,
+                share_price float,
+                amount float,
+                spend float,
+                shift float,
+                spread float
+            ) 
+        """
+        conn.execute(table_query)
+        conn.execute(f"copy {table_name} from '{import_file}';")
