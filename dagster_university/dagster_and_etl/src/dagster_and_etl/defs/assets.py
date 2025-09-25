46593200731181fbd132cba426a7c459845f0503
jsiryj <jsiryj@gmail.com>
2025-09-25 17:30:19 +0000

    Add import_file asset

---
+++ dagster_university/dagster_and_etl/src/dagster_and_etl/defs/assets.py
@@ -0,0 +1,12 @@
+import dagster as dg
+from pathlib import Path
+
+class IngestionFileConfig(dg.Config):
+    path: str
+
+@dg.asset()
+def import_file(context: dg.AssetExecutionContext, config: IngestionFileConfig) -> str:
+    file_path = (
+        Path(__file__).absolute().parent / f"../../../data/source/{config.path}"
+    )
+    return str(file_path.resolve())
